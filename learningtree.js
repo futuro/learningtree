beff8976dc1b755d13eb61d15f6c976cf306d80f
futuro <eniessenderry@gmail.com>
2015-09-12 15:31:20 -0500

    Basic marker zoom groups work

--- learningtree.js
+++ learningtree.js
@@ -1,7 +1,45 @@
+window.markerLayers={};
+
+function onMapClick(e) {
+    map = window.theMap;
+    mLayers = window.markerLayers;
+    zoom = map.getZoom();
+
+    if (_.isUndefined(mLayers[zoom])) {
+        mLayers[zoom]=L.layerGroup();
+    }
+    marker = L.marker(e.latlng);
+    mLayers[zoom].addLayer(marker);
+}
+
+function onZoomEnd(e) {
+    map = window.theMap;
+    mLayers = window.markerLayers;
+    zoom = map.getZoom();
+
+    if (! _.isUndefined(mLayers[zoom])){
+        map.addLayer(mLayers[zoom]);
+    }
+}
+
+function onZoomStart(e){
+    map = window.theMap;
+    mLayers = window.markerLayers;
+    zoom = map.getZoom();
+
+    if (! _.isUndefined(mLayers[zoom])){
+        map.removeLayer(mLayers[zoom]); 
+    }
+}
+
 function createMap() {
-    var map = L.map('map').setView([44.973333, -93.266667], 12),
-        topoLayer = {};
-        window.ccMap = map;
+    var map = L.map('map', {
+                    center: [44.973333, -93.266667],
+                    zoom: 12,
+                    maxBounds: [[44.86463, -93.42773],
+                                [45.12587, -93.03497]]});
+    window.theMap = map;
+
 
     // This is kind of busy, but OSM is cool and I like the map otherwise
     // http://{s}.tile.osm.org/{z}/{x}/{y}.png
@@ -17,8 +55,12 @@ function createMap() {
             '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
             }).addTo(map);
 
-    //var infoObjs = constructDoD(map);
-    //topoLayer = constructTopoLayer(map, infoObjs.info);
+    map.on('click', onMapClick);
+    map.on('zoomend', onZoomEnd);
+    map.on('zoomstart',onZoomStart);
 }
+
 // Make sure that the DOM is available, then do map related stuff
 $( document ).ready(createMap);
+
+// vim: set et ts=4 sw=4
